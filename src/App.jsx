fe6f8f60d6cc3c24fa76591a7bd199721a3c4b7a
unknown <codingataya@gmail.com>
2026-08-29 17:17:32 +0700

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Games from './pages/Games';
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Games />} />
         <Route path="/catalog" element={<Home />} />
